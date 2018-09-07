b1ecd826c58fbee0a054b4301e98c1c6265c14c3
Ryan Wiebe <wieberockin@yahoo.com>
2018-09-06 20:19:14 -0600

    Added code from video

--- UnderstandingScope/Program.cs
+++ UnderstandingScope/Program.cs
@@ -8,10 +8,57 @@ namespace UnderstandingScope
 {
     class Program
     {
+        private static string k = "";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            string j = "";
+
+            for (int i = 0; i < 10; i++)
+            {
+                j = i.ToString();
+                k = i.ToString();
+                Console.WriteLine(i);
+
+                if (i == 9)
+                {
+                    string l = i.ToString();
+                }
+                //This one is not available becaue it was defined inside the for loop.
+                //Console.WriteLine(l);
+
+            }
+
+            //Console.WriteLine(i);
+            Console.WriteLine("Outside of the for: " + j);
+            Console.WriteLine("Outside of the for: " + k);
+
+            HelperMethod();
+
+            Car myCar = new Car();
+            myCar.DoSomething();
+            
+
+            Console.ReadLine();
+        }
+
+        static void HelperMethod()
+        {
+            Console.WriteLine("Value of k from the Helper Method(): " + k);
         }
     }
+
+    class Car
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine(helperMethod());
+        }
+        private string helperMethod()
+        {
+            return "Hello World!";
+        }
+    }
+
+
 }
